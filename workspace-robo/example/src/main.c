00dbcf788bd9e452e0d3b493c24cc125c7817807
Christoph Kepler <christoph.kepler@arts.aero>
2014-03-04 15:50:40 +0100

    Updated main

--- workspace-robo/example/src/main.c
+++ workspace-robo/example/src/main.c
@@ -1,5 +1,19 @@
 #include "../h/main.h"
-#define PORT NXT_PORT_S1
+
+/**
+ * Define S Ports
+ */
+#define S1 NXT_PORT_S1
+#define S2 NXT_PORT_S2
+#define S3 NXT_PORT_S3
+#define S4 NXT_PORT_S4
+
+/**
+ * Define alphabetical Ports
+ */
+#define A NXT_PORT_A
+#define B NXT_PORT_B
+#define C NXT_PORT_C
 
 /// DO NOT DELETE THIS METHOD
 /// It is called every 1ms and e.g. can be used for implementing a
@@ -8,66 +22,55 @@ void user_1ms_isr_type2(void) {
 }
 
 void ecrobot_device_initialize() {
-	 ecrobot_set_light_sensor_active(PORT);
+	 ecrobot_set_light_sensor_active(S1);
 }
 
 void ecrobot_device_terminate(void) {
-	ecrobot_set_light_sensor_inactive(PORT);
+	ecrobot_set_light_sensor_inactive(S1);
 }
 
-void hello_world() {
-	ecrobot_status_monitor("Hello, World!");
+void start_robo() {
+	nxt_motor_set_speed(B, -65, 0);
+	nxt_motor_set_speed(C, -65, 0);
+}
 
+void stop_robo() {
+	nxt_motor_set_speed(B, 0, 1);
+	nxt_motor_set_speed(C, 0, 1);
+}
+
+void beep() {
+	ecrobot_sound_tone(300,500,40);
 }
 
 TASK(OSEK_Main_Task) {
-	/*while (1) {
-		hello_world();
-		/* 500msec wait */
-		/*systick_wait_ms(500);
-	}*/
-
 	while(1) {
-		nxt_motor_set_speed(NXT_PORT_B, -65, 0);
-		nxt_motor_set_speed(NXT_PORT_C, -65, 0);
-	      display_clear(0);
-	      display_goto_xy(0,0);
-	      display_int(ecrobot_get_light_sensor(PORT),4);
-	      display_update();
-	      systick_wait_ms(500);
-	      if(ecrobot_get_light_sensor(NXT_PORT_S1) <= 600) {
-	    	  nxt_motor_set_speed(NXT_PORT_B, 0, 1);
-	    	  nxt_motor_set_speed(NXT_PORT_C, 0, 1);
-	    	  break;
-	      }
-	      if(ecrobot_get_touch_sensor(NXT_PORT_S2) == 1 || ecrobot_get_touch_sensor(NXT_PORT_S3) == 1) {
-	    	  nxt_motor_set_speed(NXT_PORT_B, 0, 1);
-	    	  nxt_motor_set_speed(NXT_PORT_C, 0, 1);
+		display_clear(0);
+		display_goto_xy(0,0);
+		display_int(ecrobot_get_light_sensor(S1),4);
+		display_update();
+		systick_wait_ms(500);
 
-	      }
+
+		if(ecrobot_get_light_sensor(S1) <= 600) {
+			stop_robo();
+		}
+		else {
+			start_robo();
+		}
+
+		if(ecrobot_get_touch_sensor(S2) == 1 || ecrobot_get_touch_sensor(S3) == 1) {
+			stop_robo();
+			beep();
+		}
+		else {
+			start_robo();
+		}
 	}
-
+	/**
+	 * Prevent state unclear if breaking while
+	 */
 	while(1) {
 		systick_wait_ms(50);
 	}
-
 }
-/*TASK(task1) {
-	while(1) {
-		if (ecrobot_is_ENTER_button_pressed()){
-			nxt_motor_set_speed(PORT, 50, 0);
-		}
-	}
-}*/
-
-TASK(task2) {
-  while(1) {
-    if (ecrobot_is_ENTER_button_pressed()){
-      display_clear(0);
-      display_goto_xy(0,0);
-      display_int(ecrobot_get_light_sensor(PORT),4);
-      display_update();
-    }
-  }
-}
-
